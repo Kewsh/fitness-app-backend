4bbd1acff98a105fe42ab34a6ce9879bcaaa3e58
Reza Kharazminia <Reza.kharazmi1379@gmail.com>
2022-12-02 11:02:09 +0330

    Implement Reset password page (#11)
    - Implement the reset password page
    - Add scrollview to the SignUp as an athlete layout

---
+++ frontend/FitnessClubApp/app/src/main/java/com/fitness/app/views/fragments/ResetPasswordFragment.kt
@@ -0,0 +1,36 @@
+package com.fitness.app.views.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.NavController
+import com.fitness.app.R
+import com.fitness.app.databinding.FragmentResetPasswordBinding
+
+class ResetPasswordFragment : Fragment(R.layout.fragment_reset_password) {
+    lateinit var binding: FragmentResetPasswordBinding
+    lateinit var navigator: NavController
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        return inflater.inflate(R.layout.fragment_reset_password, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.backButton.setOnClickListener {
+            navigator.popBackStack()
+        }
+    }
+}
