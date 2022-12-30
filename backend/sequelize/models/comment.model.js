4f15d7bc5575b727b4d5fc7b3562970006eee2d8
Koosha Kazemi <kooshakazemi@gmail.com>
2022-12-30 16:58:33 +0330

    Fix beforeCreate hook logic in comment model

--- backend/sequelize/models/comment.model.js
+++ backend/sequelize/models/comment.model.js
@@ -15,11 +15,14 @@ module.exports = (sequelize) => {
         },
     }, {
         hooks: {
-            beforeCreate: comment => {
-                if (!comment.programId &&
-                    !comment.eventId &&
-                    !comment.dietId &&
-                    !comment.recipeReviewId)
+            beforeCreate: ({programId, eventId, dietId, recipeReviewId}) => {
+                // make sure only one of the IDs is defined
+                if ([
+                        programId ? 1 : 0,
+                        eventId ? 1 : 0,
+                        dietId ? 1 : 0,
+                        recipeReviewId ? 1 : 0
+                    ].reduce((a,b) => a+b, 0) != 1)
                 {
                     throw new Error('Comment must belong to either a program, event, diet, or recipe review');
                 }
