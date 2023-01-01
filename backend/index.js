b356a37a928ffc0973c25ec90c81c52f87a187dd
Koosha Kazemi <kooshakazemi@gmail.com>
2023-01-01 23:20:07 +0330

    Implement app.js and index.js files to run the server

---
+++ backend/index.js
@@ -0,0 +1,28 @@
+const app = require('./express/app');
+const sequelize = require('./sequelize');
+const PORT = 3000;
+
+const assertDatabaseConnectionOk = async () => {
+	console.log(`Checking database connection...`);
+	try {
+		await sequelize.authenticate();
+		console.log('Database connection OK!');
+	} catch (error) {
+		console.log('Unable to connect to the database:');
+		console.log(error.message);
+		process.exit(1);
+	}
+}
+
+async function init() {
+	await assertDatabaseConnectionOk();
+
+    // synchronize models with database
+    await sequelize.sync({ force: true });
+
+	app.listen(PORT, () => {
+		console.log(`Server started listening on port ${PORT}`);
+	});
+}
+
+init();
