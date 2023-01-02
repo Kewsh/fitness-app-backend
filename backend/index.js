db67d376e462e501a764d891722d11e71e11a8e1
Koosha Kazemi <kooshakazemi@gmail.com>
2023-01-02 10:42:15 +0330

    Change indentation from tabs to spaces in index.js

--- backend/index.js
+++ backend/index.js
@@ -3,26 +3,26 @@ const sequelize = require('./sequelize');
 const PORT = 3000;
 
 const assertDatabaseConnectionOk = async () => {
-	console.log(`Checking database connection...`);
-	try {
-		await sequelize.authenticate();
-		console.log('Database connection OK!');
-	} catch (error) {
-		console.log('Unable to connect to the database:');
-		console.log(error.message);
-		process.exit(1);
-	}
+    console.log(`Checking database connection...`);
+    try {
+        await sequelize.authenticate();
+        console.log('Database connection OK!');
+    } catch (error) {
+        console.log('Unable to connect to the database:');
+        console.log(error.message);
+        process.exit(1);
+    }
 }
 
 async function init() {
-	await assertDatabaseConnectionOk();
+    await assertDatabaseConnectionOk();
 
     // synchronize models with database
     await sequelize.sync({ force: true });
 
-	app.listen(PORT, () => {
-		console.log(`Server started listening on port ${PORT}`);
-	});
+    app.listen(PORT, () => {
+        console.log(`Server started listening on port ${PORT}`);
+    });
 }
 
 init();
