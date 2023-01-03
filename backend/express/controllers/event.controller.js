60b4603a9a8640f1d8c945d08d27ebdcd4bf016e
Koosha Kazemi <kooshakazemi@gmail.com>
2023-01-03 21:32:16 +0330

    Implement all event route handlers except discover and getCoverPicture

---
+++ backend/express/controllers/event.controller.js
@@ -0,0 +1,70 @@
+const {
+    event: eventModel,
+    club: clubModel,
+    user: userModel,
+} = require('../../sequelize').models;
+
+module.exports.discover = async (req, res) => {
+
+}
+
+module.exports.findOneById = async (req, res) => {
+    try {
+        const event = await eventModel.findByPk(req.params.id, {
+            attributes: { exclude: ['coverPicPath'] },
+            include: {
+                model: clubModel,
+                attributes: ['name'],
+            },
+        });
+        if (!event) {
+            return res.status(404).json('No event found with this id');
+        }
+        return res.status(200).json(event);
+    } catch (error) {
+        return res.status(500).json(error);
+    }
+}
+
+module.exports.getComments = async (req, res) => {
+    try {
+        const comments = await commentModel.findAll({
+            where: { eventId: req.params.id }
+        });
+        return res.status(200).json(comments);
+    } catch (error) {
+        return res.status(500).json(error);
+    }
+}
+
+module.exports.getCoverPicture = async (req, res) => {
+
+}
+
+module.exports.participate = async (req, res) => {
+    try {
+        if (!req.body.userId) {
+            return res.status(400).json(
+                'Missing field "userId" in request body'
+            );
+        }
+        // can we do this all in one query?
+        const event = await eventModel.findByPk(req.params.id);
+        if (!event) {
+            return res.status(404).json('No event found with this id');
+        }
+        const user = await userModel.findByPk(req.body.userId);
+        if (!user) {
+            return res.status(404).json('No user found with this id');
+        }
+        const participation = await event.addUser(user);
+        if (!participation) {
+            return res.status(400).json(
+                'User has already participated in this event'
+            );
+        }
+        return res.status(200).json(participation);
+    } catch (error) {
+        return res.status(500).json(error);
+    }
+}
--- backend/express/routes/api/event.routes.js
+++ backend/express/routes/api/event.routes.js
@@ -1,27 +1,20 @@
 const router = require('express').Router();
+const {
+    discover,
+    findOneById,
+    getComments,
+    getCoverPicture,
+    participate
+} = require('../../controllers/event.controller');
 
-router.get('/', (req, res) => {
-    // get event suggestions
-});
+router.get('/', discover);
 
-router.get('/:id', (req, res) => {
-    // get one
-});
+router.get('/:id', findOneById);
 
-router.get('/:id/club', (req, res) => {
-    // get event's club
-});
+router.get('/:id/comments', getComments);
 
-router.get('/:id/comments', (req, res) => {
-    // get event's comments
-});
+router.get('/:id/cover', getCoverPicture);
 
-router.get('/:id/cover', (req, res) => {
-    // get event's cover picture
-});
-
-router.post('/:id/participate', (req, res) => {
-    // participate in an event
-});
+router.post('/:id/participate', participate);
 
 module.exports = router;
