7d74888ce193e3d353e6f9e0708f760015f7fd72
Koosha Kazemi <kooshakazemi@gmail.com>
2023-01-03 20:19:35 +0330

    Implement all diet route handlers except discover and getCoverPicture

---
+++ backend/express/controllers/diet.controller.js
@@ -0,0 +1,91 @@
+const {
+    diet: dietModel,
+    nutritionist: nutritionistModel,
+    food: foodModel,
+    recipe: recipeModel,
+    comment: commentModel,
+    user: userModel,
+} = require('../../sequelize').models;
+
+module.exports.discover = async (req, res) => {
+
+}
+
+module.exports.findOneById = async (req, res) => {
+    try {
+        const diet = await dietModel.findByPk(req.params.id, {
+            attributes: { exclude: ['coverPicPath'] },
+            include: {
+                model: nutritionistModel,
+                attributes: { exclude: ['picPath'] }
+            },
+        });
+
+        if (!diet) {
+            return res.status(404).json('No diet found with this id');
+        }
+        return res.status(200).json(diet);
+    } catch (error) {
+        return res.status(500).json(error);
+    }
+}
+
+module.exports.getCoverPicture = async (req, res) => {
+
+}
+
+// this route must have query params (at least day)
+module.exports.getFoods = async (req, res) => {
+    try {
+        const foods = await foodModel.findAll({
+            where: { dietId: req.params.id }
+        });
+        return res.status(200).json(foods);
+    } catch (error) {
+        return res.status(500).json(error);
+    }
+}
+
+module.exports.getRecipes = async (req, res) => {
+    try {
+        const recipes = await recipeModel.findAll({
+            where: { dietId: req.params.id }
+        });
+        return res.status(200).json(recipes);
+    } catch (error) {
+        return res.status(500).json(error);
+    }
+}
+
+module.exports.getComments = async (req, res) => {
+    try {
+        const comments = await commentModel.findAll({
+            where: { dietId: req.params.id }
+        });
+        return res.status(200).json(comments);
+    } catch (error) {
+        return res.status(500).json(error);
+    }
+}
+
+module.exports.pick = async (req, res) => {
+    try {
+        if (!req.body.userId) {
+            return res.status(400).json(
+                'Missing field "userId" in request body'
+            );
+        }
+        const [ affectedRows ] = await userModel.update({
+            dietId: req.params.id,
+        }, {
+            where: { id: req.body.userId }
+        });
+        if (!affectedRows) {
+            return res.status(404).json('No user found with this id');
+        }
+
+        return res.status(200).json();
+    } catch (error) {
+        return res.status(500).json(error);
+    }
+}
