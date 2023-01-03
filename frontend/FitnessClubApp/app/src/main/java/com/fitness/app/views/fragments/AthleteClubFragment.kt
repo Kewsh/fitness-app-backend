761010e6c65ff52158ca4989eb9c359a81b28bcb
Reza <Reza.kharazmi1379@gmail.com>
2023-01-03 23:20:28 +0330

    Implemented athlete club page

---
+++ frontend/FitnessClubApp/app/src/main/java/com/fitness/app/views/fragments/AthleteClubFragment.kt
@@ -0,0 +1,78 @@
+package com.fitness.app.views.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.activityViewModels
+import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import com.fitness.app.R
+import com.fitness.app.adapters.DiscoverProgramsAdapter
+import com.fitness.app.adapters.YourEventsAdapter
+import com.fitness.app.databinding.FragmentAthleteClubBinding
+import com.fitness.app.databinding.FragmentAthleteDiscoverBinding
+import com.fitness.app.databinding.FragmentAthleteHomeBinding
+import com.fitness.app.viewmodel.AthleteHomeViewModel
+import com.fitness.app.views.activities.AthleteHomeActivity
+import java.util.concurrent.TimeUnit
+
+class AthleteClubFragment : Fragment(R.layout.fragment_athlete_club) {
+    val viewModel: AthleteHomeViewModel by activityViewModels()
+    lateinit var binding: FragmentAthleteClubBinding
+    lateinit var eventsAdapter: YourEventsAdapter
+    lateinit var programAdapter: DiscoverProgramsAdapter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding = FragmentAthleteClubBinding.bind(view)
+        val activity = activity as AthleteHomeActivity
+
+        setUpPrograms()
+        setUpEvents()
+    }
+
+    private fun setUpPrograms(){
+        binding.programsRecyclerView.apply {
+            layoutManager = StaggeredGridLayoutManager(1, StaggeredGridLayoutManager.HORIZONTAL)
+            setHasFixedSize(true)
+            programAdapter =
+                DiscoverProgramsAdapter(viewLifecycleOwner, context)
+//            dietAdapter.stateRestorationPolicy = RecyclerView.Adapter.StateRestorationPolicy.PREVENT_WHEN_EMPTY
+            adapter = programAdapter
+            postponeEnterTransition(300, TimeUnit.MILLISECONDS)
+            viewTreeObserver.addOnPreDrawListener {
+                startPostponedEnterTransition()
+                true
+            }
+
+        }
+
+        programAdapter.submitList(viewModel.getAllDiscoverProgramsItems())
+    }
+
+    private fun setUpEvents(){
+        binding.eventsRecyclerView.apply {
+            layoutManager = StaggeredGridLayoutManager(1, StaggeredGridLayoutManager.HORIZONTAL)
+            setHasFixedSize(true)
+            eventsAdapter = YourEventsAdapter(viewLifecycleOwner, context)
+//            dietAdapter.stateRestorationPolicy = RecyclerView.Adapter.StateRestorationPolicy.PREVENT_WHEN_EMPTY
+            adapter = eventsAdapter
+            postponeEnterTransition(300, TimeUnit.MILLISECONDS)
+            viewTreeObserver.addOnPreDrawListener {
+                startPostponedEnterTransition()
+                true
+            }
+
+        }
+
+        eventsAdapter.submitList(viewModel.getAllYourEventsItems())
+    }
+
+}
