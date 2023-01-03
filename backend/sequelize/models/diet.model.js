65135b9c6614d0d8a03fd5d4cc7effc31290cb12
Koosha Kazemi <kooshakazemi@gmail.com>
2023-01-03 20:18:00 +0330

    Add new virtual fields and afterFind hook to diet model
    - numberOfUsers
    - rating
    - numberOfRatings

--- backend/sequelize/models/diet.model.js
+++ backend/sequelize/models/diet.model.js
@@ -19,5 +19,45 @@ module.exports = (sequelize) => {
                 min: 0,
             },
         },
+        numberOfUsers: {
+            type: DataTypes.VIRTUAL,
+        },
+        rating: {
+            type: DataTypes.VIRTUAL,
+        },
+        numberOfRatings: {
+            type: DataTypes.VIRTUAL,
+        }
+    }, {
+        hooks: {
+            afterFind: async query => {
+                // set virtual fields
+                query.numberOfUsers = await getNumberOfUsers(
+                    sequelize,
+                    query.id
+                );
+                const rating = await getRating(sequelize, query.id);
+                query.rating = Number(rating.dataValues.avgRate);
+                query.numberOfRatings = Number(rating.dataValues.nRates);
+            }
+        }
     });
+}
+
+
+const getRating = async (sequelize, dietId) => {
+    // find all comments whose dietId is the same as param dietId
+    // this query returns a list with one member (why?)
+    return (await sequelize.models.comment.findAll({
+        where: { dietId },
+        attributes: [
+            [sequelize.fn('AVG', sequelize.col('rate')), 'avgRate'],
+            [sequelize.fn('COUNT', sequelize.col('rate')), 'nRates'],
+        ],
+    }))[0];
+}
+
+const getNumberOfUsers = async (sequelize, dietId) => {
+    // get number of users whose dietId is the same as param dietId
+    return await sequelize.models.user.count({ where: { dietId } });
 }
