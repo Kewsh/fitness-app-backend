33fa673df11c022762f5de8ac99d1d1867567024
Koosha Kazemi <kooshakazemi@gmail.com>
2023-01-16 02:00:44 +0330

    Implement multer/upload related utils

---
+++ backend/express/consts.js
@@ -0,0 +1,7 @@
+module.exports = {
+    // directory to keep user uploaded files
+    uploadsDirectory: 'uploads',
+
+    // maximum uploadable image size
+    maxImageSize: 5 * 1024 * 1024,   // 5 MB
+}
