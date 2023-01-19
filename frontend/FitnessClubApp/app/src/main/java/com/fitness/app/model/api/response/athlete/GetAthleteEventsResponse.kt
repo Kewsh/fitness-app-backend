693297ad5b26a0669ef46b50de12c66273c5dc5c
Reza <Reza.kharazmi1379@gmail.com>
2023-01-19 15:24:14 +0330

    Implement other athlete apis models

---
+++ frontend/FitnessClubApp/app/src/main/java/com/fitness/app/model/api/response/athlete/GetAthleteEventsResponse.kt
@@ -0,0 +1,16 @@
+package com.fitness.app.model.api.response.athlete
+
+import com.fitness.app.model.api.response.event.Club
+import com.google.gson.annotations.SerializedName
+
+data class GetAthleteEventsResponse(
+    @SerializedName("status") val status:String,
+    @SerializedName("code") val code:Int,
+    @SerializedName("data") val data:ArrayList<GetAthleteEventsData>,
+)
+
+data class GetAthleteEventsData(
+    @SerializedName("id") val id:Int,
+    @SerializedName("title") val title:String,
+    @SerializedName("club") val club: Club,
+)
