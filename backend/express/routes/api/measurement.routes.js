eca7ecf8e9ddf16ed0e415244e2db0073bb15b34
Koosha Kazemi <kooshakazemi@gmail.com>
2023-01-23 16:04:14 +0330

    Delete measurement route and controller files

--- backend/express/routes/api/measurement.routes.js
+++
@@ -1,6 +0,0 @@
-const router = require('express').Router();
-const { updateOne } = require('../../controllers/measurement.controller');
-
-router.patch('/:id', updateOne);
-
-module.exports = router;
