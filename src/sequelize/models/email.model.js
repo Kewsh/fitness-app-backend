e4175387d5b1775a82fd6be70a999997d7699d6a
Koosha Kazemi <kooshakazemi@gmail.com>
2023-02-01 12:01:12 +0330

    Set isLowercase validation on email to prevent bypassing unique property (#6)

--- src/sequelize/models/email.model.js
+++ src/sequelize/models/email.model.js
@@ -8,6 +8,7 @@ module.exports = (sequelize) => {
             unique: true,
             validate: {
                 isEmail: true,
+                isLowercase: true,
             },
         },
     }, {
